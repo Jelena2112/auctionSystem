dc1bd892fe40de15d79d843797804ccbf51703c6
PC <dijanic@gmail.com>
2024-04-09 23:09:45 +0200

    * Show all auctions

---
+++ app/Models/Auction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+class Auction extends BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function createAuction(int $productId, int $owner): void
+    {
+        $productId = $this->sql->real_escape_string($productId);
+        $owner = $this->sql->real_escape_string($owner);
+        $this->sql->query("INSERT INTO auction (product_id, owner) VALUES ($productId, $owner)");
+    }
+    public function getAll(): array
+    {
+        return $this->sql->query("
+            SELECT a.latest_bid, u.email as 'owner', p.name as 'product', uo.email as 'won_by' FROM auction AS a
+                INNER JOIN users AS u ON u.id = a.owner
+                INNER JOIN products AS p ON p.id = a.product_id
+                LEFT JOIN users AS uo ON uo.id = a.won_by
+        ")->fetch_all(MYSQLI_ASSOC);
+    }
+}
---
+++ auctions.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Auction;
+
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$auctionModel = new Auction();
+?>
+
+
+<!DOCTYPE HTML>
+
+<html>
+
+    <head></head>
+
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Owner</th>
+                    <th>Latest bid</th>
+                    <th>Won by</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach($auctionModel->getAll() as $auction): ?>
+                    <tr>
+                        <td><?= $auction['product'] ?></td>
+                        <td><?= $auction['owner'] ?></td>
+                        <td><?= $auction['latest_bid'] ?></td>
+                        <td><?= $auction['won_by'] ?></td>
+                        <td><a href="bid.php">Bid</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </body>
+</html>

0064609e30c11ec7d46e78ad0897aa97db9003da
PC <dijanic@gmail.com>
2024-04-09 22:38:29 +0200

    * Added registration & db_schema.txt

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,25 @@
 namespace App\Models;
 
 
+use mysqli;
+
 class User extends BaseModel
 {
-    public function test(): void
+    public function __construct()
     {
-        echo "HELLO WORLD";
-
+        parent::__construct();
+    }
+    public function userExists(string $email): bool
+    {
+        $email = $this->sql->real_escape_string($email);
+        $response = $this->sql->query("SELECT id FROM users WHERE email='$email' LIMIT 1");
+        return $response->num_rows >= 1;
+    }
+    public function register(string $email, string $password): void
+    {
+        $password = password_hash(password: $password,algo: PASSWORD_DEFAULT);
+        $email = $this->sql->real_escape_string($email);
+        $password = $this->sql->real_escape_string($password);
+        $this->sql->query("INSERT INTO users (email, password) VALUES ('$email', '$password')");
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);

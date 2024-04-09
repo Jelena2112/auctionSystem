6452b046ee1849a770f79d6a0fa783fad19de14d
PC <dijanic@gmail.com>
2024-04-09 23:10:02 +0200

    * Comment out create auctions

--- index.php
+++ index.php
@@ -6,3 +6,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+//$auctionModel = new \App\Models\Auction();
+//
+//
+//for($i = 0; $i < 20; $i++) {
+//    $auctionModel->createAuction(
+//        productId: rand(1,5),
+//        owner: rand(1,2)
+//    );
+//}
